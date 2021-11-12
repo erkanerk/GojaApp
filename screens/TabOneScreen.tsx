bf1664efb6d48d56d53b1e92892b8a1bd50ad7dd
Maja Danielsson <danielsson.maja96@gmail.com>
2021-11-12 15:53:04 +0100

    Started implementing component, not done

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import { LoginRegisterButtons } from '../components/LoginRegisterButtons/LoginRegisterButtons';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
@@ -9,6 +10,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <LoginRegisterButtons email={"test"} username={"teeest"} password={"pass"}/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
