589fc68cc561f09ca1abb8f288a9dad495703944
Maja Danielsson <danielsson.maja96@gmail.com>
2021-11-17 10:58:19 +0100

    Broken out code

--- components/Forms/RegisterForm/RegisterForm.tsx
+++ components/Forms/RegisterForm/RegisterForm.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, TextInput, Keyboard, Text, View } from "react-native";
 import { SubmitButton } from '../SubmitButton';
 import {APIKit, setClientToken} from '../../../shared/APIkit';
 import Spinner from 'react-native-loading-spinner-overlay';
-import { validateEmail, validateUserName, validatePassword } from "./Utils";
+import { emptyFormErrors, validateForm } from "./Utils";
 
 const styles = StyleSheet.create({
     input: {
@@ -31,38 +31,24 @@ const styles = StyleSheet.create({
 });
 
 export const RegisterForm = () => {
-      
     const [email, setEmail] = useState("");
-    const [emailError, setEmailError] = useState("");
     const [userName, setUserName] = useState("");
-    const [userNameError, setUserNameError] = useState("");
     const [password, setPassword] = useState("");
-    const [passwordError, setPasswordError] = useState("");
-      const [isLoading, setIsLoading] = useState(false);
+    const [formErrors, setFormErrors] = useState(emptyFormErrors);
+    const [isLoading, setIsLoading] = useState(false);
 
     const onSubmit = async () => {
-        const {emailIsValid, emailErrorMessage} = validateEmail(email);
-        const {usernameIsValid, usernameErrorMessage} = validateUserName(userName);
-        const {passwordIsValid, passwordErrorMessage} = validatePassword(password);
-
-        if (emailIsValid && usernameIsValid && passwordIsValid) {
+        const {isValid, formErrors} = validateForm(email, userName, password);
+        setFormErrors(formErrors);
+        if (isValid) {
             Keyboard.dismiss();
             await registerUser();
-            setEmailError("");
-            setUserNameError("");
-            setPasswordError("");
-        } else {
-            setEmailError(emailErrorMessage);
-            setUserNameError(usernameErrorMessage);
-            setPasswordError(passwordErrorMessage);
         }
     }
 
     const registerUser = async () => {
         const payload = {email, userName, password};
-
         setIsLoading(true);
-
         APIKit.post('/users/signup/', payload)
             .then((response) => {
                 setClientToken(response.data.token);
@@ -74,45 +60,45 @@ export const RegisterForm = () => {
             });
     }
 
-      return (
-        <>
-            <Spinner visible={isLoading} />
-            <View style={styles.formWrapper}>
-                <View style={styles.inputField}>
-                    <TextInput
-                        style={[styles.input, emailError ? styles.faultyInput : null]}
-                        value={email}
-                        placeholder={"Email"}
-                        onChangeText={(text) => setEmail(text)}
-                        autoCapitalize={"none"}
-                    />
-                    {emailError ? <Text style={styles.errorMessage}>{emailError}</Text> : null}
-                </View>
-                <View style={styles.inputField}>
-                    <TextInput
-                        style={[styles.input, userNameError ? styles.faultyInput : null]}
-                        value={userName}
-                        placeholder={"Username"}
-                        onChangeText={(text) => setUserName(text)}
-                        autoCapitalize={"none"}
-                    />
-                    {userNameError ? <Text style={styles.errorMessage}>{userNameError}</Text> : null}
-                </View>
-                <View style={styles.inputField}>
-                    <TextInput
-                        style={[styles.input, passwordError ? styles.faultyInput : null]}
-                        value={password}
-                        placeholder={"Password"}
-                        secureTextEntry
-                        onChangeText={(text) => setPassword(text)}
-                    />
-                    {passwordError ? <Text style={styles.errorMessage}>{passwordError}</Text> : null}
-                </View>
+    return (
+    <>
+        <Spinner visible={isLoading} />
+        <View style={styles.formWrapper}>
+            <View style={styles.inputField}>
+                <TextInput
+                    style={[styles.input, formErrors.emailErrorMessage ? styles.faultyInput : null]}
+                    value={email}
+                    placeholder={"Email"}
+                    onChangeText={(text) => setEmail(text)}
+                    autoCapitalize={"none"}
+                />
+                {formErrors.emailErrorMessage ? <Text style={styles.errorMessage}>{formErrors.emailErrorMessage}</Text> : null}
             </View>
-            <SubmitButton
-                text={"Register"}
-                onPress={onSubmit}
-            />
-        </>
-      );
+            <View style={styles.inputField}>
+                <TextInput
+                    style={[styles.input, formErrors.usernameErrorMessage ? styles.faultyInput : null]}
+                    value={userName}
+                    placeholder={"Username"}
+                    onChangeText={(text) => setUserName(text)}
+                    autoCapitalize={"none"}
+                />
+                {formErrors.usernameErrorMessage ? <Text style={styles.errorMessage}>{formErrors.usernameErrorMessage}</Text> : null}
+            </View>
+            <View style={styles.inputField}>
+                <TextInput
+                    style={[styles.input, formErrors.passwordErrorMessage ? styles.faultyInput : null]}
+                    value={password}
+                    placeholder={"Password"}
+                    secureTextEntry
+                    onChangeText={(text) => setPassword(text)}
+                />
+                {formErrors.passwordErrorMessage ? <Text style={styles.errorMessage}>{formErrors.passwordErrorMessage}</Text> : null}
+            </View>
+        </View>
+        <SubmitButton
+            text={"Register"}
+            onPress={onSubmit}
+        />
+    </>
+    );
 }; 
--- components/Forms/RegisterForm/Utils.tsx
+++ components/Forms/RegisterForm/Utils.tsx
@@ -1,4 +1,29 @@
-export const validateEmail = (email: string) : {emailIsValid: boolean, emailErrorMessage: string} => {
+export interface IFormErrors {
+    emailErrorMessage?: string;
+    usernameErrorMessage?: string;
+    passwordErrorMessage?: string;
+}
+
+export const emptyFormErrors = (): IFormErrors => {
+    return {
+        emailErrorMessage: '',
+        usernameErrorMessage: '',
+        passwordErrorMessage: ''
+    }
+}
+
+export const validateForm = (email: string, userName: string, password: string) : {isValid: boolean, formErrors: IFormErrors} => {
+    const {emailIsValid, emailErrorMessage} = validateEmail(email);
+    const {usernameIsValid, usernameErrorMessage} = validateUserName(userName);
+    const {passwordIsValid, passwordErrorMessage} = validatePassword(password);
+
+    if (emailIsValid && usernameIsValid && passwordIsValid) {
+        return {isValid: true, formErrors: emptyFormErrors()}
+    } 
+    return {isValid: false, formErrors: {emailErrorMessage: emailErrorMessage, usernameErrorMessage: usernameErrorMessage, passwordErrorMessage: passwordErrorMessage}}
+}
+
+const validateEmail = (email: string) : {emailIsValid: boolean, emailErrorMessage: string} => {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(email)) {
        return {emailIsValid: true, emailErrorMessage: ""};
@@ -7,7 +32,7 @@ export const validateEmail = (email: string) : {emailIsValid: boolean, emailErro
     return {emailIsValid: false, emailErrorMessage: "Invalid email address"}; 
 };
 
-export const validateUserName = (userName: string) : {usernameIsValid: boolean, usernameErrorMessage: string} => {
+const validateUserName = (userName: string) : {usernameIsValid: boolean, usernameErrorMessage: string} => {
     if (userName.trim().length < 3 || userName.trim().length > 20) {
         return {usernameIsValid: false, usernameErrorMessage: "Username should be between 3-20 characters long"}; 
     }
@@ -15,7 +40,7 @@ export const validateUserName = (userName: string) : {usernameIsValid: boolean, 
     return {usernameIsValid: true, usernameErrorMessage: ""};
 }
 
-export const validatePassword = (password: string) : {passwordIsValid: boolean, passwordErrorMessage: string} => {
+const validatePassword = (password: string) : {passwordIsValid: boolean, passwordErrorMessage: string} => {
     if (password.trim().length < 6) {
         return {passwordIsValid: false, passwordErrorMessage: "Password should be at least 6 characters long"};
     } 
