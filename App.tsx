c55d88ff6bf4dfa7bdc9de827ae963c133c705c3
Erik <36402235+erkanerk@users.noreply.github.com>
2021-11-22 15:27:01 +0100

    auth rerouting and stuff

--- App.tsx
+++ App.tsx
@@ -1,22 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import AppContext from "./shared/AppContext";
 
 export default function App() {
     const isLoadingComplete = useCachedResources();
     const colorScheme = useColorScheme();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const appCtx = {
+        loggedIn,
+        setLoggedIn,
+    };
+
     if (!isLoadingComplete) {
         return null;
     } else {
         return (
-            <SafeAreaProvider>
-                <Navigation colorScheme={colorScheme} />
-                <StatusBar />
-            </SafeAreaProvider>
+            <AppContext.Provider value={appCtx}>
+                <SafeAreaProvider>
+                    <Navigation colorScheme={colorScheme} />
+                    <StatusBar />
+                </SafeAreaProvider>
+            </AppContext.Provider>
         );
     }
 }
