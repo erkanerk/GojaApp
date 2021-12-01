bf763ee4560e9afd2a7c0cddc9cc6912aa8ddc38
Niklas Sundqvist <niklas.l.sundqvist@gmail.com>
2021-12-01 13:42:18 +0100

    Fix in styling

--- components/PlayCard/subcomponents/Reply.tsx
+++ components/PlayCard/subcomponents/Reply.tsx
@@ -1,42 +1,38 @@
 import React from 'react';
 import { StyleSheet, View, Pressable } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faReply } from '@fortawesome/free-solid-svg-icons'
+
+import { faReply } from '@fortawesome/free-solid-svg-icons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    alignContent: 'center',
-    alignSelf: 'center',
-    flexDirection: 'row',
-  },
-  image: {
-    width: 20,
-    height: 20,
-  },
-}); 
+    container: {
+        alignItems: 'center',
+        alignContent: 'center',
+        alignSelf: 'center',
+        flexDirection: 'row',
+    },
+    image: {
+        width: 20,
+        height: 20,
+    },
+});
 
 // TODO: This whole component man
 
 interface Props {
-    post: Post
+    post: Post;
 }
 
-export const Reply = ({ 
-    post
-}: Props) => {
-
+export const Reply = ({ post }: Props) => {
     function handleOnPress() {
-        console.log("Reply button pressed")
+        console.log('Reply button pressed');
     }
-    
+
     return (
-    <View style={styles.container}>
-      <Pressable
-        onPress={handleOnPress}
-        >
-        <FontAwesomeIcon icon={ faReply } size={ 25 } color="black"/>
-      </Pressable>
-    </View>
+        <View style={styles.container}>
+            <Pressable onPress={handleOnPress}></Pressable>
+        </View>
     );
-}; 
+};
+
+/* <Icon class="fas fa-reply" size={25} color="black" />*/
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,35 +1,12 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import { RecordButton } from '../components/Record/RecordButton';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { View } from '../components/Themed';
 import { RecordingScreen } from './postFlow/RecordingScreen';
 import { PostType } from './postFlow/RecordingScreen';
 
 export default function TabTwoScreen() {
     return (
-        <View style={styles.container}>
+        <View>
             <RecordingScreen recordingScreenType={PostType.POST} />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
-});
