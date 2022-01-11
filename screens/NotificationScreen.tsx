77a40e4e789db880c863e52f64c35507872bd5b2
Martin Dannelind <martin.dannelind@gmail.com>
2022-01-11 15:49:26 +0100

    Removed sample data and added a placeholder

--- screens/NotificationScreen.tsx
+++ screens/NotificationScreen.tsx
@@ -7,7 +7,8 @@ import { RouteProp, useIsFocused } from "@react-navigation/native";
 import AppContext from "../shared/AppContext";
 import { APIKit, onFailure } from "../shared/APIkit";
 import { NotificationEvent } from "../components/NotificationEvent/NotificationEvent";
-import { SampleNotifications } from "../assets/sampleData/Notifications";
+import { FadeText } from "../components/FadeText/FadeText";
+
 
 export const styles = StyleSheet.create({
     container: {
@@ -17,7 +18,16 @@ export const styles = StyleSheet.create({
     },
     feedView: {
         marginTop: 10,
-    }
+    },
+    textView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 12,
+        color: 'gray',
+    },
 });
 
 interface Props {
@@ -32,17 +42,13 @@ export default function NotificationScreen({
     const globalCtx = useContext(AppContext);
     const isFocused = useIsFocused();
     const [isLoading, setIsLoading] = useState<boolean>(false)
-    // TODO: rm SampleNotifications 
-    const [notifications, setNotifications] = useState<Notification[] | undefined>(SampleNotifications);
+    const [notifications, setNotifications] = useState<Notification[] | undefined>(undefined);
     
     // TODO: change to a real endpoint
     async function getNotifications() {
         setIsLoading(true)
-        //console.log('Fetching notifications')
         APIKit.get("/notifications/me")
         .then((response) => {
-            //console.log("Successful /users/followers/:id response: ")
-            //console.log(response.data)
             setNotifications(response.data);
             setIsLoading(false);
         })
@@ -66,12 +72,17 @@ export default function NotificationScreen({
 
     return (
     <View style={styles.container}>
+        {!notifications ? 
+        <View style={styles.textView}>
+            <FadeText style={styles.text}>You have no new notifications</FadeText>
+        </View>
+        :
         <View style={styles.feedView}>
             <FlatList
             data={notifications}
             keyExtractor={notification => notification.id}
             renderItem={renderItem} />
-        </View>
+        </View>}
     </View>
     );
 }
